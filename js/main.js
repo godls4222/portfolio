af06fd53dbd80da43743ada3f770496b23218177
godls4222 <72494486+godls4222@users.noreply.github.com>
2020-11-27 12:45:53 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,25 +2,40 @@
 
 /* ability animation */
 enterView({
-    selector: '.ability',  // 화면에 진입했을때 애니메이션이 될 요소명
+    selector: '.ani_1',  // 화면에 진입했을때 애니메이션이 될 요소명
     offset: 0.5,          // 시작 지연시간(ms)
     enter: function(el) {
         el.classList.add('play');  // 애니메이션 클래스 명(CSS)
     },
 });
 
-/* enterView({ 
-    selector: '.block2', 
+enterView({ 
+    selector: '.ani_2', 
     offset: 0.5, 
     enter: function(el) { 
-        el.classList.add('fade-slide-right'); 
+        el.classList.add('play'); 
     },
 });  
 
 enterView({
-    selector: '.block3',
+    selector: '.ani_3',
     offset: 0.5,
     enter: function(el) {
-        el.classList.add('fade-slide-down');
+        el.classList.add('play');
     },
-});  */
+});
+
+enterView({
+    selector: '.ani_4',
+    offset: 0.5,
+    enter: function(el) {
+        el.classList.add('play');
+    },
+});
+
+/* 자동 스크롤 */
+function fnMove(seq){
+    var offset = $("section" + seq).offset();
+    $('html, body').animate({scrollTop : offset.top},400);
+}
+
